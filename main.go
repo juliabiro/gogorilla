494b4df1b6bc3717d04ba1ea70cc8f336a2458ea
Julia Biro <julia.biro@gmail.com>
2020-10-10 17:18:25 +0200

    gorillas on rooftops

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	"log"
-
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"image/color"
 	_ "image/png"
+	"log"
 	"math/rand"
+	"time"
 )
 
 const (
-	screenWidth  = 640
-	screenHeight = 480
+	screenWidth  = 1200
+	screenHeight = 700
 
 	frameOX     = 0
 	frameOY     = 32
@@ -22,8 +23,9 @@ const (
 
 // Game implements ebiten.Game interface.
 type Game struct {
-	gorilla1 Gorilla
-	gorilla2 Gorilla
+	gorilla1  Gorilla
+	gorilla2  Gorilla
+	buildings []Building
 }
 
 // Update proceeds the game state.
@@ -36,6 +38,10 @@ func (g *Game) Update(screen *ebiten.Image) error {
 // Draw draws the game screen.
 // Draw is called every frame (typically 1/60[s] for 60Hz display).
 func (g *Game) Draw(screen *ebiten.Image) {
+
+	for _, b := range g.buildings {
+		b.Draw(screen)
+	}
 	g.gorilla1.Draw(screen)
 	g.gorilla2.Draw(screen)
 	// Write your game's rendering.
@@ -43,14 +49,15 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 // Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
 // If you don't have to adjust the screen size with the outside size, just return a fixed size.
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 640, 480
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
 }
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	game := &Game{}
 	// Sepcify the window size as you like. Here, a doulbed size is specified.
-	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("Gorillas")
 	setup(game)
 	// Call ebiten.RunGame to start your game loop.
@@ -78,17 +85,68 @@ func (g *Gorilla) Draw(screen *ebiten.Image) {
 	screen.DrawImage(g.img, op)
 }
 
-func setup(g *Game) {
-	g.gorilla1 = Gorilla{Point{rand.Intn(200), 200 + rand.Intn(200)}, true, nil, 50, 50}
-	g.gorilla2 = Gorilla{Point{300 + rand.Intn(200), 200 + rand.Intn(200)}, true, nil, 50, 50}
+type Building struct {
+	Point
+	img    *ebiten.Image
+	height int
+	width  int
+	color  color.Color
+}
 
+func (b *Building) Draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+
+	op.GeoM.Translate(float64(b.X), float64(b.Y))
+	b.img.Fill(b.color)
+
+	screen.DrawImage(b.img, op)
+}
+
+func setupBuildings(g *Game) {
+	k := 0
+	for k < screenWidth {
+		w := 100 + rand.Intn(screenWidth/12)
+		h := 150 + rand.Intn(screenHeight/2)
+		if k+w >= screenWidth {
+			w = screenWidth - k
+		}
+		img, _ := ebiten.NewImage(w, h, ebiten.FilterDefault)
+		g.buildings = append(g.buildings, Building{Point{k, screenHeight - h}, img, h, w, color.RGBA{0, 0, 100 + uint8(rand.Intn(155)), 255}})
+		k = k + w
+	}
+}
+
+func (g *Gorilla) init(minx int, b []Building) {
+
+	g.alive = true
 	var err error
-	g.gorilla1.img, _, err = ebitenutil.NewImageFromFile("/Users/juliabiro/go/gorilla/gorilla.png", ebiten.FilterDefault)
+	g.img, _, err = ebitenutil.NewImageFromFile("/Users/juliabiro/go/gorilla/gorilla.png", ebiten.FilterDefault)
 	if err != nil {
 		log.Fatal(err)
 	}
-	g.gorilla2.img, _, err = ebitenutil.NewImageFromFile("/Users/juliabiro/go/gorilla/gorilla.png", ebiten.FilterDefault)
-	if err != nil {
-		log.Fatal(err)
+	g.width = 50
+	g.height = 50
+	g.X = minx + rand.Intn(0.6*screenWidth/2)
+
+	// find my rooftop
+	i := 0
+	for g.X > b[i].X+b[i].width {
+		i++
 	}
+	bb := b[i]
+
+	// make sure I sit on it
+	g.Y = bb.Y - g.height
+	if g.X < bb.X || g.X+g.width > bb.X+bb.width {
+		g.X = bb.X + rand.Intn(bb.width-g.width)
+	}
+}
+func setupGorillas(g *Game) {
+	g.gorilla1.init(0, g.buildings)
+	g.gorilla2.init(screenWidth/2, g.buildings)
+}
+
+func setup(g *Game) {
+	setupBuildings(g)
+	setupGorillas(g)
 }
