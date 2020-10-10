6aa489dc3c0dbb07779031067cd6bc41364fcb31
Julia Biro <julia.biro@gmail.com>
2020-10-10 20:58:26 +0200

    prettier windwos

--- main.go
+++ main.go
@@ -86,25 +86,25 @@ func (g *Gorilla) Draw(screen *ebiten.Image) {
 }
 
 type Windows struct {
-	color             color.Color
-	width             int
-	height            int
-	spacingHorizontal int
-	spacingVertical   int
-	img               *ebiten.Image
+	color            color.Color
+	width            int
+	height           int
+	borderHorizontal int
+	borderVertical   int
+	img              *ebiten.Image
 }
 
 func (w *Windows) Draw(screen *ebiten.Image, b *Building) {
 	op := &ebiten.DrawImageOptions{}
 
 	w.img.Fill(w.color)
-	scaleX := float64(w.width) / float64(w.spacingHorizontal)
-	scaleY := float64(w.height) / float64(w.spacingVertical)
-	for i := 0; (i+1)*(w.spacingHorizontal)-w.width < b.width; i++ {
-		for j := 0; (j+1)*(w.spacingVertical)-w.height < b.height; j++ {
+	scaleX := float64(w.width-2*w.borderHorizontal) / float64(w.width)
+	scaleY := float64(w.height-2*w.borderVertical) / float64(w.height)
+	for i := 1; i*w.width-w.borderHorizontal < b.width; i++ {
+		for j := 1; j*w.height-w.borderVertical < b.height; j++ {
 			op.GeoM.Reset()
 			op.GeoM.Scale(scaleX, scaleY)
-			op.GeoM.Translate(float64(b.X+(i+1)*w.spacingHorizontal-w.width), float64(b.Y+(j+1)*w.spacingVertical-w.height))
+			op.GeoM.Translate(float64(b.X+(i-1)*w.width+w.borderHorizontal), float64(b.Y+(j-1)*w.height+w.borderVertical))
 			screen.DrawImage(w.img, op)
 		}
 	}
@@ -138,13 +138,13 @@ func setupBuildings(g *Game) {
 		}
 		img, _ := ebiten.NewImage(w, h, ebiten.FilterDefault)
 		c := color.RGBA{0, 0, 100 + uint8(rand.Intn(155)), 255}
-		wc := color.RGBA{0, 100, 100 + uint8(rand.Intn(155)), 255}
-		ww := 5 + rand.Intn(10)
-		wh := 15 + rand.Intn(20)
-		sv := ww + 3 + rand.Intn(5)
-		sh := wh + 3 + rand.Intn(5)
+		wc := color.RGBA{100 + uint8(rand.Intn(155)), 100 + uint8(rand.Intn(155)), 0, 255}
+		ww := w / (5 + rand.Intn(8))
+		bh := ww * (rand.Intn(15) + 8) / 50
+		wh := h / (5 + rand.Intn(15))
+		bv := wh * (rand.Intn(15) + 8) / 50
 		wimg, _ := ebiten.NewImage(ww, wh, ebiten.FilterDefault)
-		windows := Windows{wc, ww, wh, sv, sh, wimg}
+		windows := Windows{wc, ww, wh, bh, bv, wimg}
 		g.buildings = append(g.buildings, Building{Point{k, screenHeight - h}, img, h, w, c, windows})
 		k = k + w
 	}
