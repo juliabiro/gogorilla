07fc822524183b5a9b4860fdc534606166466ad2
Julia Biro <julia.biro@gmail.com>
2020-11-05 19:13:28 +0100

    table test for banaana.Out

--- gorilla/banana_test.go
+++ gorilla/banana_test.go
@@ -27,3 +27,33 @@ func TestResetBanana(t *testing.T) {
 		t.Fatalf("banana gravity coordinate not reset properly")
 	}
 }
+
+func TestOut(t *testing.T) {
+	var bananaLocationTestCases = []struct {
+		coordinates [2]float64
+		out         bool
+	}{
+		// inside the image
+		{[2]float64{0.0, 0.0}, false},
+		{[2]float64{5.0, 4.0}, false},
+		{[2]float64{ScreenWidth / 2, ScreenHeight / 2}, false},
+		// out left
+		{[2]float64{-1.0, 0.0}, true},
+		// out right
+		{[2]float64{ScreenWidth + 5, 0.0}, true},
+		// out up
+		{[2]float64{0, -100.0}, false},
+		// out down
+		{[2]float64{0, ScreenHeight + 5}, true},
+	}
+
+	b := NewBanana()
+
+	for _, tc := range bananaLocationTestCases {
+		b.X = tc.coordinates[0]
+		b.Y = tc.coordinates[1]
+		if b.Out() != tc.out {
+			t.Fatalf("coordinates %f,%f should be out: %t", tc.coordinates[0], tc.coordinates[1], tc.out)
+		}
+	}
+}
